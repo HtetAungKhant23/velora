470eb5b19548409a96a1e813f942faa386e312bd
Htet Aung Khant <htetaung23.ha@gmail.com>
2026-05-28 20:24:35 +0630

    feat(user-domain): impl new user and verify password method

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/HtetAungKhant23/velora
 go 1.25.0
 
 require (
-	github.com/google/uuid v1.6.0 // indirect
-	golang.org/x/crypto v0.52.0 // indirect
+	github.com/google/uuid v1.6.0
+	golang.org/x/crypto v0.52.0
 )
--- internal/core/domain/user/user.go
+++ internal/core/domain/user/user.go
@@ -19,3 +19,33 @@ type User struct {
 	createdAt time.Time
 	updatedAt time.Time
 }
+
+func NewUser(rawEmail, plainPassword string) (*User, error) {
+	email, err := NewEmail(rawEmail)
+	if err != nil {
+		return nil, err
+	}
+
+	hashedPassword, err := NewHashedPassword(plainPassword)
+	if err != nil {
+		return nil, err
+	}
+
+	now := time.Now().UTC()
+	user := &User{
+		id:        newUserID(),
+		email:     email,
+		password:  hashedPassword,
+		createdAt: now,
+		updatedAt: now,
+	}
+
+	return user, nil
+}
+
+func (u *User) VerifyPassword(plainText string) error {
+	if err := u.password.Verify(plainText); err != nil {
+		return err
+	}
+	return nil
+}
